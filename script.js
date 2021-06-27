57de60753965fa014c1a749ca080785a5f313979
dorian <dorianlegan@gmail.com>
2021-06-27 20:32:21 +0100

    added slider to change grid cell count, set grid to initialise at 9 x 9

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-let number = parseInt(prompt("How many cells"));
-
+let number = 9;
+let range = document.querySelector("#myRange");
+range.addEventListener("change", rangeChange);
 let sketchBox = document.querySelector("#sketchBox");
 sketchBox.addEventListener("click", sketchClick);
+updateGrid(number);
+
+function rangeChange(e) {
+
+    console.log(this.value)
+    number = this.value;
+    updateGrid(number);
+
+}
+
+
+function updateGrid(num) {
+
+
+    sketchBox.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    sketchBox.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+    for (i = 0; i < num * num; i++) {
+        let currentCell = document.createElement("div");
+        currentCell.classList.toggle("cell");
+        sketchBox.appendChild(currentCell);
+    }
+
+
+
+}
+
 
 function sketchClick(e) {
 
     console.log(e.target);
 }
-
-sketchBox.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
-sketchBox.style.gridTemplateRows = `repeat(${number}, 1fr)`;
-
-
-
-for(i = 0; i < number * number; i++) {
-
-
-    let currentCell = document.createElement("div");
-    currentCell.classList.toggle("cell");
-    sketchBox.appendChild(currentCell);
-    
-}
-
-
-
-
-
