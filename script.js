ad71bbc6a60fbe7cbbc47a334e4ff3c22e6d537a
dorian <dorianlegan@gmail.com>
2021-06-28 13:19:18 +0100

    added lock button

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 let number = 5;
+let color = "greyColor";
+let eraserON = false;
+let locked = false;
+
+let lockIcon = document.querySelector("#lockIcon");
+
+
+
 let range = document.querySelector("#myRange");
 range.addEventListener("change", rangeChange);
+
 let sketchBox = document.querySelector("#sketchBox");
-sketchBox.addEventListener("click", sketchClick);
-updateGrid(number);
+sketchBox.addEventListener("mouseover", sketchClick);
+
+sketchBox.addEventListener("click", lockGrid);
+
+let resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", resetGrid);
+
+
+let eraser = document.querySelector("#eraser");
+let greyColor = document.querySelector("#greyScale");
+greyColor.addEventListener("click", greyScale);
+eraser.addEventListener("click",erase);
 
 function rangeChange(e) {
 
     console.log(this.value)
     number = this.value;
     updateGrid(number);
+    resetGrid();
 
 }
 
@@ -32,5 +52,64 @@ function updateGrid(num) {
 
 function sketchClick(e) {
 
-    console.log(e.target);
+    if(!locked) {
+
+   
+    let currentCell = e.target;
+    console.log(currentCell.style);
+
+    if (eraserON) {
+
+        currentCell.classList.remove(color);
+    }
+
+    else {
+    
+    currentCell.classList.add(color);
+
+    }
+
 }
+}
+
+
+function resetGrid() {
+
+    let cellList = document.querySelectorAll(".cell");
+    console.log(cellList);
+    cellList.forEach(function(element){
+
+        element.classList.remove("greyColor");
+
+
+    });
+
+
+}
+
+function greyScale() {
+
+    eraserON = false;
+}
+
+function erase() {
+
+    eraserON = true;
+}
+
+
+function lockGrid() {
+
+    if(locked == false) {
+
+        locked = true;
+        lockIcon.style.opacity = "100";
+        return;
+    }
+
+    locked = false;
+    lockIcon.style.opacity = "0";
+}
+
+updateGrid(number);
+resetGrid();
